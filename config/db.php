b65aa27337d9bb6eb51900bce0e3739c99692a59
Milot Azemi <Milot.Azemi@it.htl-hl.ac.at>
2026-03-06 08:54:46 +0100

    created and fixed  yii project but still in WIP

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=db;dbname=HausaufgabenPlaner',
+    'username' => 'db',
+    'password' => 'db',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
