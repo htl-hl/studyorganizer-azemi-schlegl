e3f53c28cd8b2ff3e12a3c92fda3759e3d68d79e
Milot Azemi <Milot.Azemi@it.htl-hl.ac.at>
2026-03-13 09:11:30 +0100

    trying to fix some things

--- views/hausaufgabe/index.php
+++ views/hausaufgabe/index.php
@@ -31,7 +31,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
             'ID',
             'Titel',
-            'Beschr:ntext',
+                'Beschr',
             'Fachname',
             'Erledigt',
             //'Faelligkeitsdatum',
